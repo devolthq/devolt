b3ca0b6422c777ae67f7de04499fac5e7fe24226
Henrique Marlon <henriquemarloncs@gmail.com>
2024-05-31 03:28:36 -0300

    create admin management functions and deployer contract

--- cmd/rollup/main.go
+++ cmd/rollup/main.go
@@ -3,37 +3,36 @@ package main
 import (
 	"context"
 	"crypto/ecdsa"
-	"strconv"
-	"strings"
-
-	// "crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"log"
 	"log/slog"
+	"strconv"
+	"strings"
 
 	"github.com/devolthq/devolt/configs"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/jmoiron/sqlx"
-
-	// "github.com/ethereum/go-ethereum/common"
 	"github.com/devolthq/devolt/internal/domain/entity"
 	"github.com/devolthq/devolt/internal/infra/repository"
 	"github.com/devolthq/devolt/internal/usecase"
 	"github.com/devolthq/devolt/internal/usecase/dto"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/jmoiron/sqlx"
 	"github.com/rollmelette/rollmelette"
 )
 
 type DeVoltRollup struct {
-	State     *sqlx.DB
-	Admin     *common.Address
-	PublicKey *ecdsa.PublicKey
+	State                 *sqlx.DB
+	Admins                []*common.Address
+	TokenAddress          *common.Address
+	DeployerPluginAddress *common.Address
+	PublicKey             *ecdsa.PublicKey
 }
 
-func NewDeVoltRollup(state *sqlx.DB, admin *common.Address, publicKey *ecdsa.PublicKey) *DeVoltRollup {
+func NewDeVoltRollup(state *sqlx.DB, admins []*common.Address, publicKey *ecdsa.PublicKey) *DeVoltRollup {
 	return &DeVoltRollup{
 		State:     state,
-		Admin:     admin,
+		Admins:    admins,
 		PublicKey: publicKey,
 	}
 }
@@ -79,14 +78,98 @@ func (d *DeVoltRollup) Advance(
 	updateStationUseCase := usecase.NewUpdateStationUseCase(stationRepository)
 	// deleteStationUseCase := usecase.NewDeleteStationUseCase(stationRepository)
 
+	////////////////////////// Auxiliar Functions //////////////////////////
+	hasRole := func(sender common.Address) bool {
+		for _, v := range d.Admins {
+			if v == &sender {
+				return true
+			}
+		}
+		return false
+	}
+
 	///////////////////////// Router //////////////////////////
 	switch input.Kind {
-	case "BuyEnergy":
-		log.Printf("Rolling Buy: %v", string(input.Payload))
-	case "SellEnergy":
-		log.Printf("Rolling Sell: %v", string(input.Payload))
-	case "FinishAuction":
-		log.Printf("Rolling Finish: %v", string(input.Payload))
+	// case "BuyEnergy":
+	// 	log.Printf("Rolling Buy: %v", string(input.Payload))
+	// case "SellEnergy":
+	// 	log.Printf("Rolling Sell: %v", string(input.Payload))
+	// case "FinishAuction":
+	// 	log.Printf("Rolling Finish: %v", string(input.Payload))
+	case "TokenAddress":
+		var NewTokenAddress common.Address
+		if err := json.Unmarshal(input.Payload, &NewTokenAddress); err != nil {
+			return fmt.Errorf("failed to unmarshal new token address as address: %w", err)
+		}
+		if hasRole(metadata.MsgSender); !hasRole(metadata.MsgSender) {
+			return fmt.Errorf("sender is not an admin: %v", metadata.MsgSender)
+		}
+		d.TokenAddress = &NewTokenAddress
+		env.Report([]byte(fmt.Sprintf("set token address to: %v", NewTokenAddress)))
+	case "DeployerPluginAddress":
+		var NewDeployerPluginAddress common.Address
+		if err := json.Unmarshal(input.Payload, &NewDeployerPluginAddress); err != nil {
+			return fmt.Errorf("failed to unmarshal new deployer plugin address as address: %w", err)
+		}
+		if hasRole(metadata.MsgSender); !hasRole(metadata.MsgSender) {
+			return fmt.Errorf("sender is not an admin: %v", metadata.MsgSender)
+		}
+		d.DeployerPluginAddress = &NewDeployerPluginAddress
+		env.Report([]byte(fmt.Sprintf("set deployer plugin address to: %v", NewDeployerPluginAddress)))
+	case "DeployContract":
+		if hasRole(metadata.MsgSender); !hasRole(metadata.MsgSender) {
+			return fmt.Errorf("sender is not an admin: %v", metadata.MsgSender)
+		}
+		abiJson := `[{
+										"inputs":[
+											{
+													"internalType":"bytes",
+													"name":"_bytecode",
+													"type":"bytes"
+											}
+										],
+										"stateMutability":"payable",
+										"type":"function",
+										"name":"deployAnyContract",
+										"outputs":[
+											{
+													"internalType":"address",
+													"name":"addr",
+													"type":"address"
+											}
+										]
+								}]`
+		abiInterface, err := abi.JSON(strings.NewReader(abiJson))
+		if err != nil {
+			return fmt.Errorf("failed to parse abi: %w", err)
+		}
+		voucher, err := abiInterface.Pack("deployAnyContract", input.Payload)
+		if err != nil {
+			return fmt.Errorf("failed to pack abi: %w", err)
+		}
+		env.Voucher(*d.DeployerPluginAddress, voucher)
+	case "GrantAdminRole":
+		var NewOwner common.Address
+		if err := json.Unmarshal(input.Payload, &NewOwner); err != nil {
+			return fmt.Errorf("failed to unmarshal new owner as address: %w", err)
+		}
+		if hasRole(metadata.MsgSender); !hasRole(metadata.MsgSender) {
+			return fmt.Errorf("sender is not an admin: %v", metadata.MsgSender)
+		}
+		d.Admins = append(d.Admins, &NewOwner)
+		env.Report([]byte(fmt.Sprintf("granted admin role to: %v", NewOwner)))
+	case "RevokeAdminRole":
+		if hasRole(metadata.MsgSender); !hasRole(metadata.MsgSender) {
+			return fmt.Errorf("sender is not an admin: %v", metadata.MsgSender)
+		}
+		var remainingOwners []*common.Address
+		for _, v := range d.Admins {
+			if v != &metadata.MsgSender {
+				remainingOwners = append(remainingOwners, v)
+			}
+		}
+		d.Admins = remainingOwners
+		env.Report([]byte(fmt.Sprintf("revoked admin role from: %v", metadata.MsgSender)))
 	case "DeviceReport":
 		////////////////////////// Decode Report //////////////////////////
 		var report *entity.Report
@@ -126,26 +209,27 @@ func (d *DeVoltRollup) Advance(
 			env.Report(outputBytes)
 			log.Printf("Station created: %v", output.Id)
 			return nil
+		} else {
+			//////////////////////// Update Station //////////////////////////
+			output, err := updateStationUseCase.Execute(&usecase.UpdateStationInputDTO{
+				Id:        station.Id,
+				Rate:      station.Rate + (payload.Rate / 30),
+				Owner:     payload.Wallet,
+				State:     "active",
+				Latitude:  payload.Latitude,
+				Longitude: payload.Longitude,
+				UpdatedAt: metadata.BlockTimestamp,
+			})
+			if err != nil {
+				return fmt.Errorf("failed to update station: %w", err)
+			}
+			outputBytes, err := json.Marshal(output)
+			if err != nil {
+				return fmt.Errorf("failed to marshal output: %w", err)
+			}
+			env.Report(outputBytes)
+			log.Printf("Station updated: %v", output.Id)
 		}
-		//////////////////////// Update Station //////////////////////////
-		output, err := updateStationUseCase.Execute(&usecase.UpdateStationInputDTO{
-			Id:        station.Id,
-			Rate:      station.Rate + (payload.Rate / 30),
-			Owner:     payload.Wallet,
-			State:     "active",
-			Latitude:  payload.Latitude,
-			Longitude: payload.Longitude,
-			UpdatedAt: metadata.BlockTimestamp,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to update station: %w", err)
-		}
-		outputBytes, err := json.Marshal(output)
-		if err != nil {
-			return fmt.Errorf("failed to marshal output: %w", err)
-		}
-		env.Report(outputBytes)
-		log.Printf("Station updated: %v", output.Id)
 	default:
 		return fmt.Errorf("invalid input: %v", input)
 	}
@@ -154,7 +238,7 @@ func (d *DeVoltRollup) Advance(
 
 func (d *DeVoltRollup) Inspect(env rollmelette.EnvInspector, payload []byte) error {
 	parameters := strings.Split(strings.TrimSpace(strings.TrimSuffix(strings.TrimPrefix(string(payload), "/"), "/")), "/")
-	
+
 	stationRepository := repository.NewStationRepositorySqlite(d.State)
 	auctionRepository := repository.NewAuctionRepositorySqlite(d.State)
 	bidRepository := repository.NewBidRepositorySqlite(d.State)
@@ -274,12 +358,16 @@ func main() {
 		log.Fatalf("Failed to open and connect to database: %v", err)
 	}
 
-	admin := common.HexToAddress("0x0000000000000000000000000000000000000000")
+	//////////////////////// Setup State //////////////////////////
+	initialOwner := common.HexToAddress("0x0000000000000000000000000000000000000000")
+
+	var owners []*common.Address
+	owners = append(owners, &initialOwner)
 
 	///////////////////////// Rollmelette //////////////////////////
 	ctx := context.Background()
 	opts := rollmelette.NewRunOpts()
-	app := NewDeVoltRollup(db, &admin, publicKey)
+	app := NewDeVoltRollup(db, owners, publicKey)
 	err = rollmelette.Run(ctx, opts, app)
 	if err != nil {
 		slog.Error("application error", "error", err)
